5c1d1e8b226607fe75caf1c94b5c1273a992aee3
dev-aghad <ghadialiarmaan@gmail.com>
2025-11-02 18:01:38 +0000

    Created factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $this->call(UsersTableSeeder::class);
+        $this->call(ProfilesTableSeeder::class);
+        $this->call(PostsTableSeeder::class);
+        $this->call(CommentsTableSeeder::class);
+        $this->call(TagsTableSeeder::class);
+        $this->call(PostsTagsTableSeeder::class);
     }
 }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,6 +13,10 @@ class UsersTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $u = new User;
+        $u->name = "Alice";
+        $u->email = "alice@gmail.com";
+        $u->password = bcrypt('password');
+        $u->save();
     }
 }
